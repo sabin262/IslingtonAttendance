e43b0659fed9b30bda3df96e768931dd47dcfc1b
Anjil Shrestha <yenjail16@gmail.com>
2018-09-27 23:39:33 +0545

    Group View

--- IslingtonAttendance/Attendance/urls.py
+++ IslingtonAttendance/Attendance/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.faculty, name='faculty'),
-    path('<int:faculty>/', views.module_list,name="module_list"),
+    path('modules/<int:faculty>/', views.module_list,name="module_list"),
     path('<int:faculty_group>/', views.group_list,name="group_list"),
 
 
