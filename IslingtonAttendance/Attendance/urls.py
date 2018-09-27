2a7d4085e778d64318aa0fbede45f15a43bed8dd
Anjil Shrestha <yenjail16@gmail.com>
2018-09-27 23:31:08 +0545

    Views work.

---
+++ IslingtonAttendance/Attendance/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.faculty, name='faculty'),
+    path('<int:faculty>/', views.module_list,name="module_list"),
+    path('<int:faculty_group>/', views.group_list,name="group_list"),
+
+
+]
--- IslingtonAttendance/Attendance/views.py
+++ IslingtonAttendance/Attendance/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+def faculty(request):
+    faculties=Faculty.objects.all()
+    return render(request, 'faculty.html', {'faculties': faculties})
+
+def module_list(request,faculty):
+    modules = Faculty_Module_Group.objects.prefetch_related('module').filter(faculty_id=faculty)
+
+
+
+    # mods = modules[0].faculty_id
+    # mods = Faculty_Module_Group.objects.prefetch_related('module_id').prefetch_related('student_id').filter
+
+    return render(request,'modules.html', {'modules':modules})
+
+
+def group_list(request,faculty_group):
+    group=Group.objects.filter(faculty_id=faculty_group)
+
+    return render(request, 'group.html', {'groups':group})
+
