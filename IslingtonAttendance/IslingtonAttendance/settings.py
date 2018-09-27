2c3817ba5d92898108e4c594d86d4c516fc8f5d0
Anjil Shrestha <yenjail16@gmail.com>
2018-09-27 12:17:28 +0545

    Views first.

--- IslingtonAttendance/IslingtonAttendance/settings.py
+++ IslingtonAttendance/IslingtonAttendance/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'islington_attendance',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '5432',
 
