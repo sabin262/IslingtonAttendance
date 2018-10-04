b181a47131eae47c681ad80749573290aaedf7ce
sabin262 <sabin262@gmail.com>
2018-10-04 09:34:41 +0545

    Fixed templated

--- IslingtonAttendance/Attendance/urls.py
+++ IslingtonAttendance/Attendance/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('modules/<int:faculty>/', views.module_list,name="module_list"),
     path('group/<slug:faculty_group>/', views.group_list,name="group_list"),
     path('students/<slug:student_group>/', views.student_list,name="student_list"),
-    path('/authenticate', views.authenticateUser, name='authenticate')
+    path('authenticate', views.authenticateUser, name='authenticate_user')
 
 
 
